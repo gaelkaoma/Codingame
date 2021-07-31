d994d07423d7748e1401257331e4e0410edd3014
gaelkaoma <gaelkaoma@gmail.com>
2021-07-31 13:57:12 +0200

    ajout de l'énoncé pour la fonction computeClosesTo

--- ComputeClosestToZero.php
+++ ComputeClosestToZero.php
@@ -1,5 +1,11 @@
 <?php
-
+/*
+cette fonction prend un tableau de température $ts en paramètre et renvoie la températures la plus proche 
+de zéro.
+Si le tableau est vide, la fonction soit renvoyer 0
+Si deux températures sont aussi proches de zéro, la fonction devra renvoyer  la température 
+positive(ex : -5 et 5, renvoyez 5)
+*/
 function computeClosestToZero(array $ts) {
   // Write your code here
   // To debug (equivalent to var_dump): error_log(var_export($var, true));
